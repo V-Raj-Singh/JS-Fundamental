213bf21a9e3b5b4bb25f8ddcc839e0f88b454cfa
V-Raj-Singh <141900768+V-Raj-Singh@users.noreply.github.com>
2024-03-21 14:44:51 +0000

    datatype done

---
+++ 01_Basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 23456
+let emailAddress = "viks@gmail.com"
+var currentPassword = "12345"
+accountCity = "jaipur"
+
+
+// accountId = 2 // not Allowed
+
+emailAddress = "vvp@gmail.com"
+currentPassword = "345678"
+accountCity =  "bangaluru"
+
+console.log(accountId);
+console.table([accountId, emailAddress, currentPassword, accountCity]);
