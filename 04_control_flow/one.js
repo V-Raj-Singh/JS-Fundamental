8cc64aabacb1aff47edc529d3e679bcbaa08ef56
V-Raj-Singh <141900768+V-Raj-Singh@users.noreply.github.com>
2024-04-24 11:35:46 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,60 @@
+// if
+// const isUserloggedIn = true
+// const temperature = 41
+
+// if ( temperature === 40 ){
+//     console.log("less than 50");
+// } else {
+//     console.log("temperature is greater than 50");
+// }
+
+// const vikashAge = 2024-1990
+// const renukaAge = 2014-1998
+
+// if (vikashAge >= 34) {
+//     console.log("He can Qulified for exam")
+// } else {
+//     console.log("not qulifed for exam");
+// }
+
+// if (vikashAge < renukaAge) {
+//     console.log("He is yunger then renuka")
+// }else if(vikashAge > renukaAge) {
+//     console.log("She is yunger then vikash")
+// }
+// console.log("Execute");
+// <, >, <=, >=, ==, !=, ===, !==
+
+// const score = 200
+// if (score > 100) {
+//     let power = "fly"
+//     console.log(`User power: ${power}`);
+// }
+
+// const balance = 1000
+
+// if (balance < 500) {
+//     console.log("less than 500");
+// } else if (balance < 750) {
+//     console.log("less than 750");
+    
+// } else if (balance < 900) {
+//     console.log("less than 750");
+    
+// } else {
+//     console.log("less than 1200");
+
+// }
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
+if (userLoggedIn && debitCard && 2==3) {
+    console.log("Allow to buy course");
+}
+
+if (loggedInFromGoogle || loggedInFromEmail) {
+    console.log("User logged in");
+}
